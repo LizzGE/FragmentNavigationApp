bf68b1915e43033fbd018b09875a0680e3ef1a09
Lizzet Garcia Estrada <lizzetgarcia180@gmail.com>
2025-05-22 13:27:13 -0600

    Fragment_navigation: Practica, pasar datos entre Fragments.

--- app/src/main/java/com/cursosant/fragmentnavigationapp/FragmentHome.kt
+++ app/src/main/java/com/cursosant/fragmentnavigationapp/FragmentHome.kt
@@ -2,12 +2,9 @@ package com.cursosant.fragmentnavigationapp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 class FragmentHome : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
